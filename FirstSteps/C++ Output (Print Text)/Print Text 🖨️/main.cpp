2db7a0d65240a0777586828123ea43254e30768d
abc0xmattyic333 <100542195+abc0xmattyic333@users.noreply.github.com>
2024-10-20 12:39:34 -0400

    Add main.cpp for C++ output example

---
+++ FirstSteps/C++ Output (Print Text)/Print Text 🖨️/main.cpp
@@ -0,0 +1,25 @@
+// Date: 2024-10-20
+// Author: Matthew (abc0xmattyic333)
+// Github: (https://github.com/abc0xmattyic333)
+// Twitter/X: (https://x.com/abc0xmattyic333)
+
+// C++ Output (Print Text)
+
+// The cout object, together with the << operator, is used to output values and print text.
+
+// Just remember to surround the text with double qoutes (""):
+
+// Example:
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello, World!";
+  cout << "I'm learning C++.";
+  cout << "Matthew, (abc0xmattyic333) is a great developer.";
+  return 0;
+}
+
+// You can add as many cout objects as you want.
+
