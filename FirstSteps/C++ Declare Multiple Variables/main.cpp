d4a65452d69ae48da03a35065d4eb7ef59ee0ce2
abc0xmattyic333 <100542195+abc0xmattyic333@users.noreply.github.com>
2024-10-20 14:36:53 -0400

    Add C++ example for declaring multiple variables

---
+++ FirstSteps/C++ Declare Multiple Variables/main.cpp
@@ -0,0 +1,21 @@
+// Date: 2024-10-20
+// Author: Matthew (abc0xmattyic333)
+// Github: (https://github.com/abc0xmattyic333)
+// Twitter/X: (https://x.com/abc0xmattyic333)
+
+// Description: C++ Declaring Multiple Variables
+
+// To declare more than one variable of the same type, use a comma-separated list:
+
+int x = 5, y = 6, z = 50;
+cout << x + y + z;
+
+// One Value to Multiple Variables
+
+// You can also assign the same value to multiple variables in one line:
+
+
+int x, y, z;
+x = y = z = 50;
+cout << x + y + z;
+
