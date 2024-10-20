8f735ede74b5c5b843fefae89afa9e8b2a9f9958
abc0xmattyic333 <abc0xmattyic333.btc@proton.me>
2024-10-20 15:33:07 +0000

    add main.cpp

---
+++ FirstSteps/FirstCppProgram/main.cpp
@@ -0,0 +1,87 @@
+// This is a single-line comment in C++.
+
+/*
+This is a multi-line comment in C++.
+*/
+
+// Date created: 2024-10-20
+// Author: Matthew (abc0xmattyic333) 
+// Github: (https://github.com/abc0xmattyic333)
+// Twitter/X: (https://x.com/abc0xmattyic333)
+
+// What is C++?
+
+/*
+C++ is used to create computer programs, and is one of the most used language in game development.
+
+C++ is a cross-platform language that can be used to create high-performance applications.
+
+C++ was developed by Bjarne Stroustrup, as an extension to the C language.
+
+C++ gives programmers a high level of control over system resources and memory.
+
+The language was updated 5 major times in 2011, 2014, 2017, 2020, and 2023 to C++11, C++14, C++17, C++20, and C++23.
+*/
+
+// Why use C++?
+
+/*
+C++ is one of the world's most popular programming languages.
+
+C++ can be found in today's operating systems, Graphical User Interfaces (GUI's), and embedded systems.
+
+C++ is an object-oriented programming language which gives a clear structure to programs and allows code to be reused, lowering development costs.
+
+C++ is portable and can be used to develop applications that can be adapted to multiple platforms.
+
+C++ is fun and easy to learn!
+
+As C++ is close to C, C# and Java, it makes it easy for programmers to switch to C++ or vice versa.
+*/
+
+// Difference between C and C++??
+
+/*
+C++ was developed as an extension of C, and both languages have almost the same syntax.
+
+The main difference between C and C++ is that C++ support classes and objects, while C does not.
+*/
+
+// Let's get started:
+
+// This tutorial aims to teach you the basics of C++.
+
+// Whenever we are finished with this tutorial, you will be able to write C++ programs and create real-life examples.
+
+// Note: This tutorial doesn't require any prior programming experience. :)
+
+/*
+To start using C++ we need a text editor. 
+By using vscode.dev as a homescreen application on my iPhone 14 Pro Max, I can connect to remote repositories that I created on my Github account.
+You can visit my Github account at the following link:(https://github.com/abc0xmattyic333)
+*/
+
+// Let's create our first C++ file
+
+// In your editor of your choose create your first C++ program by making a file named MyFirstCppProgram.cpp
+
+// Write the following code below:
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    cout << "Hello World!";
+    return 0;
+}
+
+// Don't worry if you don't understand the above code right now. 
+
+// We will go further in detail in later chapters of this course.
+
+// If you vist our assets folder you can see a png file that shows the output of the code example above.
+
+// Congratulations! You have just witten your first C++ program. :)
+
+// I told you that you would become a C++ developer. ;p
+
