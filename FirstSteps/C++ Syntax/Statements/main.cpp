6f098458fa7ab096a7db0bf193e3379b41d78bc4
abc0xmattyic333 <100542195+abc0xmattyic333@users.noreply.github.com>
2024-10-20 12:18:54 -0400

    Add initial C++ syntax and statements examples

---
+++ FirstSteps/C++ Syntax/Statements/main.cpp
@@ -0,0 +1,33 @@
+// Date: 2024-10-20
+// Author: Matthew (abc0xmattyic333)
+// Github: (https://github.com/abc0xmattyic333)
+// Twitter/X: (https://x.com/abc0xmattyic333)
+
+
+// C++ Statements
+
+/*
+A computer program is a list of "instructions" to be "executed" by a computer.
+
+In a programming language, these programming instructions are called statements.
+
+The following statement "instructs" the compiler to print the text "Hello World" to the screen:
+*/
+
+cout << "Hello, World!";
+
+// It is important that you end a statement with a semicolon ;.
+
+// If you forget the semicolon, it will cause an error and your program won't run.
+
+
+// Many Statements
+
+// Most C++ programs contain many statements.
+
+// The statements are executed, one by one, in the order that they are written in.
+
+cout << "Hello, World!";
+cout << "Have a good day!;
+
+return 0; 
